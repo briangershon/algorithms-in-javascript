cfb0927c754be95d7257caba8eb5e18cf907788d
Brian Gershon <brian.five@gmail.com>
2019-03-27 11:23:05 -0700

    Optimize Bubble Sort with fewer iterations.

--- src/bubble-sort.js
+++ src/bubble-sort.js
@@ -12,7 +12,7 @@ class BubbleSort {
   sort() {
     const length = this.data.length;
     for (let i=0; i < length; i++) {
-      for (let j=0; j < length - 1; j++) {
+      for (let j=0; j < length - 1 - i; j++) {
         if (this.data[j] > this.data[j + 1] ) {
           arraySwap(this.data, j, j+1);
         }
