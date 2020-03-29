bf46c5566dd336d81a4b2b874f91665c3ae1a5d4
Brian Gershon <brian.five@gmail.com>
2020-03-29 11:11:31 -0700

    Use my Permutations class.

--- src/katas/6-by-6-skyscaper.js
+++ src/katas/6-by-6-skyscaper.js
@@ -1,5 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import Permutation from '../permutation';
 
 /*
 "6 by 6 Skyscraper" Kata from https://www.codewars.com/kata/5679d5a3f2272011d700000d
@@ -17,54 +18,8 @@ class SixBySixSkyscraper {
     this.skyscraperRowPermutations = [];
   }
   calculateAllRows() {
-    for (let p of findNextPermutation([1, 2, 3, 4, 5, 6])) {
-      this.skyscraperRowPermutations.push(new SkyscraperRow(p));
-    }
+    this.skyscraperRowPermutations = new Permutation().permutations([1, 2, 3, 4, 5, 6]);
   }
-}
-
-function* findNextPermutation(array) {
-  let currentPerm = array.sort((a, b) => a - b);
-  yield currentPerm;
-  for (;;) {
-    const next = nextLexPermutation(currentPerm);
-    if (next === null) break;
-    yield next;
-    currentPerm = next;
-  }
-}
-
-// Algorithm reference: https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
-// given a current permutation, figure out the next one in lexicographical order
-function nextLexPermutation(a) {
-  const array = Array.from(a);
-  const i = findLongestAscendingSequenceIndex(array);
-
-  if (i >= 0) {
-    // find first character that's greater than character at i
-    for (let j = array.length - 1; j >= 0; j--) {
-      if (array[j] > array[i]) {
-        // swap first character in longest sequence and right-mode bigger character
-        [array[j], array[i]] = [array[i], array[j]];
-        // reverse suffix and return final array
-        return array.slice(0, i + 1).concat(array.slice(i + 1).sort());
-      }
-    }
-  }
-  return null;  // no more permutations, we're 100% in lexicographical order
-}
-
-// walk backward and find longest non-increasing suffix
-function findLongestAscendingSequenceIndex(array) {
-  let last = array[array.length - 1];
-  for (let i = array.length - 2; i >= 0; i--) {
-    if (array[i] >= last) {
-      last = array[i];
-    } else {
-      return i;
-    }
-  }
-  return null;
 }
 
 // store one permutation of 6 skyscrapers
@@ -76,7 +31,8 @@ class SkyscraperRow {
     this.calculate();
   }
 
-  // left: 6  -> 1, 2, 3, 4, 5, 6  <- right: 1
+  // for 1, 2, 3, 4, 5, 6 this function calculates the number
+  // of buildings seen from left and right.
   calculate() {
     let lowestBuildingSoFarLeft = 0;
     let lowestBuildingSoFarRight = 0;
