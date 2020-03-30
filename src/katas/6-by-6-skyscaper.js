a93e299a3ebff6a8ea7a6b1ddb344af9e9e3c349
Brian Gershon <brian.five@gmail.com>
2020-03-29 17:31:04 -0700

    Add show() method. Unfortunately, clues aren’t reducing possibilities much.

--- src/katas/6-by-6-skyscaper.js
+++ src/katas/6-by-6-skyscaper.js
@@ -1,7 +1,3 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import Permutation from '../permutation';
-
 /*
 "6 by 6 Skyscraper" Kata from https://www.codewars.com/kata/5679d5a3f2272011d700000d
 
@@ -13,16 +9,75 @@ Strategy:
 
 */
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import Permutation from '../permutation';
+
 class SixBySixSkyscraper {
   constructor() {
     this.skyscraperRowPermutations = [];
+    this.calculateAllRows();
   }
   calculateAllRows() {
-    this.skyscraperRowPermutations = new Permutation().permutations([1, 2, 3, 4, 5, 6]);
+    new Permutation().permutations([1, 2, 3, 4, 5, 6]).forEach((p) => {
+      this.skyscraperRowPermutations.push(new SkyscraperRow(p));
+    });
+  }
+
+  solvePuzzle(clues) {
+    this.clues = clues;
+    const rows = [];
+    const cols = [];
+    const unique = {};
+
+    for (let i = 0; i < 6; i++) {
+      const col = [clues[i], clues[17 - i]];
+      const row = [clues[i + 6], clues[23 - i]];
+      cols.push(col);
+      rows.push(row);
+      if (!unique[col]) unique[col] = [];
+      if (!unique[row]) unique[row] = [];
+    }
+
+    let count = 0;
+    for (let i = 0; i < this.skyscraperRowPermutations.length; i++) {
+      for (let u in unique) {
+        const [left, right] = u.split(',');
+        if (this.skyscraperRowPermutations[i].left === Number(left) && this.skyscraperRowPermutations[i].right === Number(right)) {
+          unique[u].push(this.skyscraperRowPermutations[i]);
+          count++;
+        }
+      }
+    }
+
+    for (let u in unique) {
+      // console.log(u);
+      unique[u].forEach(permutation => {
+        // console.log(permutation);
+      });
+    }
+
+    return [];
+  }
+
+  show() {
+    const c = this.clues;
+    console.log(`
+     ${c[0]} ${c[1]} ${c[2]} ${c[3]} ${c[4]} ${c[5]}
+   +-------------+
+ ${c[23]} | 0 0 0 0 0 0 | ${c[6]}
+ ${c[22]} | 0 0 0 0 0 0 | ${c[7]}
+ ${c[21]} | 0 0 0 0 0 0 | ${c[8]}
+ ${c[20]} | 0 0 0 0 0 0 | ${c[9]}
+ ${c[19]} | 0 0 0 0 0 0 | ${c[10]}
+ ${c[18]} | 0 0 0 0 0 0 | ${c[11]}
+   +-------------+
+     ${c[17]} ${c[16]} ${c[15]} ${c[14]} ${c[13]} ${c[12]}`);
   }
 }
 
-// store one permutation of 6 skyscrapers
+// Store one permutation of 6 skyscrapers, along with count
+// of how many skyscrapers are visible from left and right
 class SkyscraperRow {
   constructor(array) {
     this.array = array;
--- src/katas/6-by-6-skyscaper.spec.js
+++ src/katas/6-by-6-skyscaper.spec.js
@@ -26,7 +26,23 @@ describe('SkyscraperRow', () => {
 describe('SixBySixSkyscraper', () => {
   test('calculates all permutations for a line of 6 skyscrapers', () => {
     const s = new SixBySixSkyscraper();
-    s.calculateAllRows();
     expect(s.skyscraperRowPermutations.length).toEqual(720);
+  });
+  test('can solve 6x6 puzzle 1', () => {
+    const s = new SixBySixSkyscraper();
+    const clues = [
+      3, 2, 2, 3, 2, 1,
+      1, 2, 3, 3, 2, 2,
+      5, 1, 2, 2, 4, 3,
+      3, 2, 1, 2, 2, 4
+    ];
+    const result = s.solvePuzzle(clues);
+    s.show();
+    // expect(result[0]).toEqual([ 2, 1, 4, 3, 5, 6]);
+    // expect(result[1]).toEqual([ 1, 6, 3, 2, 4, 5]);
+    // expect(result[2]).toEqual([ 4, 3, 6, 5, 1, 2]);
+    // expect(result[3]).toEqual([ 6, 5, 2, 1, 3, 4]);
+    // expect(result[4]).toEqual([ 5, 4, 1, 6, 2, 3]);
+    // expect(result[5]).toEqual([ 3, 2, 5, 4, 6, 1]);
   });
 });
