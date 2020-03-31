79ca36f0dfa49466beeed8050322dd57d418d1fd
Brian Gershon <brian.five@gmail.com>
2020-03-30 22:28:38 -0700

    Logic to discover rejected boards. Remove old permutation code.

--- src/katas/6-by-6-skyscaper.js
+++ src/katas/6-by-6-skyscaper.js
@@ -8,101 +8,140 @@ Strategy:
 * Fill in the rest of the board by trying matching permuations
 
 */
-
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import Permutation from '../permutation';
-
 class SixBySixSkyscraper {
-  constructor() {
-    this.skyscraperRowPermutations = [];
-    this.calculateAllRows();
-  }
-  calculateAllRows() {
-    new Permutation().permutations([1, 2, 3, 4, 5, 6]).forEach((p) => {
-      this.skyscraperRowPermutations.push(new SkyscraperRow(p));
-    });
-  }
-
   solvePuzzle(clues) {
-    this.clues = clues;
-    const rows = [];
-    const cols = [];
-    const unique = {};
+    let board = new Board([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], clues);
+    // console.log('board.rejected()', board.rejected());
+    return board.twoDimensionalArray();
+  }
+}
 
-    for (let i = 0; i < 6; i++) {
-      const col = [clues[i], clues[17 - i]];
-      const row = [clues[i + 6], clues[23 - i]];
-      cols.push(col);
-      rows.push(row);
-      if (!unique[col]) unique[col] = [];
-      if (!unique[row]) unique[row] = [];
-    }
+// Represent a 6x6 board
+// Input is a 36 element array and clues
+class Board {
+  constructor(boardArray, cluesArray) {
+    this.board = boardArray;
+    this.clues = cluesArray;
+  }
 
+  nextBoard() {
+
+  }
+
+  // check all 24 restraints, if any fail, return true
+  rejected() {
+    if (this.rejectLine(this.clues[0], 'col-down', 0)) return true;
+    if (this.rejectLine(this.clues[1], 'col-down', 1)) return true;
+    if (this.rejectLine(this.clues[2], 'col-down', 2)) return true;
+    if (this.rejectLine(this.clues[3], 'col-down', 3)) return true;
+    if (this.rejectLine(this.clues[4], 'col-down', 4)) return true;
+    if (this.rejectLine(this.clues[5], 'col-down', 5)) return true;
+    if (this.rejectLine(this.clues[6], 'row-left', 0)) return true;
+    if (this.rejectLine(this.clues[7], 'row-left', 1)) return true;
+    if (this.rejectLine(this.clues[8], 'row-left', 2)) return true;
+    if (this.rejectLine(this.clues[9], 'row-left', 3)) return true;
+    if (this.rejectLine(this.clues[10], 'row-left', 4)) return true;
+    if (this.rejectLine(this.clues[11], 'row-left', 5)) return true;
+    if (this.rejectLine(this.clues[12], 'col-up', 5)) return true;
+    if (this.rejectLine(this.clues[13], 'col-up', 4)) return true;
+    if (this.rejectLine(this.clues[14], 'col-up', 3)) return true;
+    if (this.rejectLine(this.clues[15], 'col-up', 2)) return true;
+    if (this.rejectLine(this.clues[16], 'col-up', 1)) return true;
+    if (this.rejectLine(this.clues[17], 'col-up', 0)) return true;
+    if (this.rejectLine(this.clues[18], 'row-right', 5)) return true;
+    if (this.rejectLine(this.clues[19], 'row-right', 4)) return true;
+    if (this.rejectLine(this.clues[20], 'row-right', 3)) return true;
+    if (this.rejectLine(this.clues[21], 'row-right', 2)) return true;
+    if (this.rejectLine(this.clues[22], 'row-right', 1)) return true;
+    if (this.rejectLine(this.clues[23], 'row-right', 0)) return true;
+    return false;
+  }
+
+  // count number of skyscrapers in array
+  // shortcircuit if we go above clue value
+  countSkyscrapers(array, clue) {
+    let lowestBuilding = 0;
     let count = 0;
-    for (let i = 0; i < this.skyscraperRowPermutations.length; i++) {
-      for (let u in unique) {
-        const [left, right] = u.split(',');
-        if (this.skyscraperRowPermutations[i].left === Number(left) && this.skyscraperRowPermutations[i].right === Number(right)) {
-          unique[u].push(this.skyscraperRowPermutations[i]);
-          count++;
-        }
+    for (let i = 0; i < 6; i++) {
+      const el = array[i];
+      if (el > lowestBuilding) {
+        lowestBuilding = el;
+        count++;
+        if (count > clue) return count;
       }
     }
-
-    for (let u in unique) {
-      // console.log(u);
-      unique[u].forEach(permutation => {
-        // console.log(permutation);
-      });
-    }
-
-    return [];
+    return count;
   }
 
-  show() {
+  getColumnDown(colNumber) {
+    return [0, 1, 2, 3, 4, 5].map((i) => this.board[6 * i + colNumber]);
+  }
+
+  getColumnUp(colNumber) {
+    return this.getColumnDown(colNumber).reverse();
+  }
+
+  getRowRight(rowNumber) {
+    return [0, 1, 2, 3, 4, 5].map((i) => this.board[6 * rowNumber + i]);
+  }
+
+  getRowLeft(rowNumber) {
+    return this.getRowRight(rowNumber).reverse();
+  }
+
+  rejectLine(clue, direction, num) {
+    if (clue === 0) return false; // consider things to be good
+    let array;
+    switch (direction) {
+    case 'col-down':
+      array = this.getColumnDown(num);
+      break;
+    case 'col-up':
+      array = this.getColumnUp(num);
+      break;
+    case 'row-left':
+      array = this.getRowLeft(num);
+      break;
+    case 'row-right':
+      array = this.getRowRight(num);
+      break;
+    }
+    if (this.countSkyscrapers(array, clue) > clue) {
+      // console.log('rejected', direction, num);
+      return true;
+    }
+    return false;
+  }
+
+  // solved if board contains all numbers > 0 and is not rejected
+  solved() {
+    const allNumbers = this.board.filter((el) => el === 0).length === 0;
+    return allNumbers && !this.rejected();
+  }
+
+  twoDimensionalArray() {
+    const result = [];
+    for (let i = 0; i < 6; i++) {
+      result.push(this.board.slice(6 * i, 6 * i + 6));
+    }
+    return result;
+  }
+
+  show () {
     const c = this.clues;
+    const b = this.board;
     console.log(`
      ${c[0]} ${c[1]} ${c[2]} ${c[3]} ${c[4]} ${c[5]}
    +-------------+
- ${c[23]} | 0 0 0 0 0 0 | ${c[6]}
- ${c[22]} | 0 0 0 0 0 0 | ${c[7]}
- ${c[21]} | 0 0 0 0 0 0 | ${c[8]}
- ${c[20]} | 0 0 0 0 0 0 | ${c[9]}
- ${c[19]} | 0 0 0 0 0 0 | ${c[10]}
- ${c[18]} | 0 0 0 0 0 0 | ${c[11]}
+ ${c[23]} | ${b[0]} ${b[1]} ${b[2]} ${b[3]} ${b[4]} ${b[5]} | ${c[6]}
+ ${c[22]} | ${b[6]} ${b[7]} ${b[8]} ${b[9]} ${b[10]} ${b[11]} | ${c[7]}
+ ${c[21]} | ${b[12]} ${b[13]} ${b[14]} ${b[15]} ${b[16]} ${b[17]} | ${c[8]}
+ ${c[20]} | ${b[18]} ${b[19]} ${b[20]} ${b[21]} ${b[22]} ${b[23]} | ${c[9]}
+ ${c[19]} | ${b[24]} ${b[25]} ${b[26]} ${b[27]} ${b[28]} ${b[29]} | ${c[10]}
+ ${c[18]} | ${b[30]} ${b[31]} ${b[32]} ${b[33]} ${b[34]} ${b[35]} | ${c[11]}
    +-------------+
      ${c[17]} ${c[16]} ${c[15]} ${c[14]} ${c[13]} ${c[12]}`);
   }
 }
 
-// Store one permutation of 6 skyscrapers, along with count
-// of how many skyscrapers are visible from left and right
-class SkyscraperRow {
-  constructor(array) {
-    this.array = array;
-    this.left = 0;  // number of builds person on left side will see
-    this.right = 0; // number of builds person on right side will see
-    this.calculate();
-  }
-
-  // for 1, 2, 3, 4, 5, 6 this function calculates the number
-  // of buildings seen from left and right.
-  calculate() {
-    let lowestBuildingSoFarLeft = 0;
-    let lowestBuildingSoFarRight = 0;
-    for (let i = 0; i < this.array.length; i++) {
-      if (this.array[i] > lowestBuildingSoFarLeft) {
-        lowestBuildingSoFarLeft = this.array[i];
-        this.left++;
-      }
-      const rightIndex = this.array.length - i - 1;
-      if ((this.array[rightIndex]) > lowestBuildingSoFarRight) {
-        lowestBuildingSoFarRight = this.array[rightIndex];
-        this.right++;
-      }
-    }
-  }
-}
-
-export { SixBySixSkyscraper, SkyscraperRow };
+export { SixBySixSkyscraper, Board };
--- src/katas/6-by-6-skyscaper.spec.js
+++ src/katas/6-by-6-skyscaper.spec.js
@@ -1,33 +1,6 @@
-import { SkyscraperRow, SixBySixSkyscraper } from './6-by-6-skyscaper';
-
-describe('SkyscraperRow', () => {
-  test('6: 1 2 3 4 5 6 :1', () => {
-    const c = new SkyscraperRow([1, 2, 3, 4, 5, 6]);
-    expect(c.left).toEqual(6);
-    expect(c.right).toEqual(1);
-  });
-  test('1: 6 5 4 3 2 1 :6', () => {
-    const c = new SkyscraperRow([6, 5, 4, 3, 2, 1]);
-    expect(c.left).toEqual(1);
-    expect(c.right).toEqual(6);
-  });
-  test('2: 3 6 5 2 4 1 :4', () => {
-    const c = new SkyscraperRow([3, 6, 5, 2, 4, 1]);
-    expect(c.left).toEqual(2);
-    expect(c.right).toEqual(4);
-  });
-  test('4: 1 2 5 6 4 3 :3', () => {
-    const c = new SkyscraperRow([1, 2, 5, 6, 4, 3]);
-    expect(c.left).toEqual(4);
-    expect(c.right).toEqual(3);
-  });
-});
+import { Board, SixBySixSkyscraper } from './6-by-6-skyscaper';
 
 describe('SixBySixSkyscraper', () => {
-  test('calculates all permutations for a line of 6 skyscrapers', () => {
-    const s = new SixBySixSkyscraper();
-    expect(s.skyscraperRowPermutations.length).toEqual(720);
-  });
   test('can solve 6x6 puzzle 1', () => {
     const s = new SixBySixSkyscraper();
     const clues = [
@@ -37,12 +10,81 @@ describe('SixBySixSkyscraper', () => {
       3, 2, 1, 2, 2, 4
     ];
     const result = s.solvePuzzle(clues);
-    s.show();
-    // expect(result[0]).toEqual([ 2, 1, 4, 3, 5, 6]);
-    // expect(result[1]).toEqual([ 1, 6, 3, 2, 4, 5]);
-    // expect(result[2]).toEqual([ 4, 3, 6, 5, 1, 2]);
-    // expect(result[3]).toEqual([ 6, 5, 2, 1, 3, 4]);
-    // expect(result[4]).toEqual([ 5, 4, 1, 6, 2, 3]);
-    // expect(result[5]).toEqual([ 3, 2, 5, 4, 6, 1]);
+    expect(result[0]).toEqual([ 2, 1, 4, 3, 5, 6]);
+    expect(result[1]).toEqual([ 1, 6, 3, 2, 4, 5]);
+    expect(result[2]).toEqual([ 4, 3, 6, 5, 1, 2]);
+    expect(result[3]).toEqual([ 6, 5, 2, 1, 3, 4]);
+    expect(result[4]).toEqual([ 5, 4, 1, 6, 2, 3]);
+    expect(result[5]).toEqual([ 3, 2, 5, 4, 6, 1]);
   });
+});
+
+describe('Board', () => {
+  test('get various rows/columns in both directions', () => {
+    const b = new Board([
+      2, 1, 4, 3, 5, 6,
+      1, 6, 3, 2, 4, 5,
+      4, 3, 6, 5, 1, 2,
+      6, 5, 2, 1, 3, 4,
+      5, 4, 1, 6, 2, 3,
+      3, 2, 5, 4, 6, 1
+    ], [
+      3, 2, 2, 3, 2, 1,
+      1, 2, 3, 3, 2, 2,
+      5, 1, 2, 2, 4, 3,
+      3, 2, 1, 2, 2, 4
+    ]);
+    expect(b.getColumnDown(0)).toEqual([2, 1, 4, 6, 5, 3]);
+    expect(b.getColumnUp(0)).toEqual([2, 1, 4, 6, 5, 3].reverse());
+    expect(b.getRowRight(0)).toEqual([2, 1, 4, 3, 5, 6]);
+    expect(b.getRowLeft(0)).toEqual([2, 1, 4, 3, 5, 6].reverse());
+  });
+
+  test('countSkyscrapers()', () => {
+    const b = new Board();
+    expect(b.countSkyscrapers([1, 2, 3, 4, 5, 6], 6)).toEqual(6);
+    expect(b.countSkyscrapers([1, 2, 3, 4, 5, 6].reverse(), 6)).toEqual(1);
+    expect(b.countSkyscrapers([3, 6, 5, 2, 4, 1], 6)).toEqual(2);
+    expect(b.countSkyscrapers([3, 6, 5, 2, 4, 1].reverse(), 6)).toEqual(4);
+    expect(b.countSkyscrapers([1, 2, 5, 6, 4, 3], 6)).toEqual(4);
+    expect(b.countSkyscrapers([1, 2, 5, 6, 4, 3].reverse(), 6)).toEqual(3);
+  });
+  test('Solved', () => {
+    const b = new Board([
+      2, 1, 4, 3, 5, 6,
+      1, 6, 3, 2, 4, 5,
+      4, 3, 6, 5, 1, 2,
+      6, 5, 2, 1, 3, 4,
+      5, 4, 1, 6, 2, 3,
+      3, 2, 5, 4, 6, 1
+    ], [
+      3, 2, 2, 3, 2, 1,
+      1, 2, 3, 3, 2, 2,
+      5, 1, 2, 2, 4, 3,
+      3, 2, 1, 2, 2, 4
+    ]);
+    expect(b.rejected()).toEqual(false);
+    expect(b.solved()).toEqual(true);
+  });
+  test('Rejected', () => {
+    const b = new Board([
+      1, 2, 3, 4, 5, 6,
+      2, 3, 4, 5, 6, 1,
+      3, 4, 5, 6, 1, 2,
+      4, 5, 6, 1, 2, 3,
+      5, 6, 1, 2, 3, 4,
+      6, 1, 2, 3, 4, 5
+    ], [
+      3, 2, 2, 3, 2, 1,
+      1, 2, 3, 3, 2, 2,
+      5, 1, 2, 2, 4, 3,
+      3, 2, 1, 2, 2, 4
+    ]);
+    expect(b.rejected()).toEqual(true);
+    expect(b.solved()).toEqual(false);
+  });
+
+  // test('next board', () => {
+
+  // });
 });
