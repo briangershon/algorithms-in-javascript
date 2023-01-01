3d812461f12c14b03372b80a520b98951d15c5d9
Brian Gershon <briangershon@users.noreply.github.com>
2023-01-01 00:39:05 -0800

    Clarify with comments

--- src/merkletree.js
+++ src/merkletree.js
@@ -4,23 +4,29 @@ class MerkleTree {
     this.concat = concat;
   }
 
-  calcLayer(leaves, concat) {
-    if (leaves.length === 1) return leaves;
+  // recursively calculate hashes for each level
+  calcLayerHash(leaves, concat) {
+    if (leaves.length === 1) return leaves; // stop recursing and return final root hash
 
+    // create next layer of hashes above prior layer
     const newLeaves = [];
     while (leaves.length) {
       if (leaves.length === 3) {
+        // handle odd number of items
         newLeaves.push(this.concat(leaves.shift(), leaves.shift()));
         newLeaves.push(leaves.shift());
       } else {
+        // handle even number of items
         newLeaves.push(this.concat(leaves.shift(), leaves.shift()));
       }
     }
-    return this.calcLayer(newLeaves, concat);
+
+    return this.calcLayerHash(newLeaves, concat);
   }
 
   getRoot() {
-    return this.calcLayer(this.leaves, this.concat).at(0);
+    // return root hash
+    return this.calcLayerHash(this.leaves, this.concat).at(0);
   }
 }
 
