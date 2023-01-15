ba7acdea75685fc5ff524bf4948840374f6faa60
Brian Gershon <brian@MacBook-Pro-6.local>
2023-01-15 00:08:16 -0800

    WIP

--- src/merkletree.js
+++ src/merkletree.js
@@ -28,6 +28,38 @@ class MerkleTree {
     // return root hash
     return this.calcLayerHash(this.leaves, this.concat).at(0);
   }
+
+  calcProofLayer(leaves, concat, index) {
+    console.log("current leaves", { leaves });
+    if (leaves.length <= 1) return leaves;
+
+    const isLeft = index % 2 == 0;
+    console.log({ isLeft });
+    console.log("leaves[index]", leaves[index]);
+    if (isLeft) {
+      console.log("leaves[index+1]", leaves[index + 1]);
+    } else {
+      console.log("leaves[index-1]", leaves[index - 1]);
+    }
+
+    const newLeaves = [];
+    while (leaves.length) {
+      if (leaves.length === 3) {
+        newLeaves.push(this.concat(leaves.shift(), leaves.shift()));
+        newLeaves.push(leaves.shift());
+      } else {
+        newLeaves.push(this.concat(leaves.shift(), leaves.shift()));
+      }
+    }
+    console.log('XXX', { newLeaves });
+    return this.calcProofLayer(newLeaves, concat, Math.floor(index / 2));
+  }
+
+  getProof(index) {
+    if (index !== 2) return;
+    console.log("looking for index", index);
+    return this.calcProofLayer(this.leaves, this.concat, index);
+  }
 }
 
 export default MerkleTree;
--- src/merkletree.spec.js
+++ src/merkletree.spec.js
@@ -1,8 +1,47 @@
 // Initial test cases from Alchemy University's course
 
 import MerkleTree from "./merkletree";
+import crypto from "crypto";
 
 const concat = (a, b) => `Hash(${a} + ${b})`;
+
+// use the crypto module to create a sha256 hash from the data passed in
+function sha256(data) {
+  return crypto.createHash("sha256").update(data).digest();
+}
+
+// the concat function we use to hash together merkle leaves
+function concatHash(left, right) {
+  if (!left)
+    throw new Error(
+      "The concat function expects two hash arguments, the first was not receieved."
+    );
+  if (!right)
+    throw new Error(
+      "The concat function expects two hash arguments, the second was not receieved."
+    );
+  return sha256(Buffer.concat([left, right]));
+}
+
+// the concat function we use to show the merkle root calculation
+function concatLetters(left, right) {
+  return `Hash(${left} + ${right})`;
+}
+
+// given a proof, finds the merkle root
+function hashProof(node, proof) {
+  console.log("PROOF", node, proof);
+  console.log(proof.length)
+  if (!proof) throw new Error("Missing proof");
+  let data = sha256(node);
+  for (let i = 0; i < proof.length; i++) {
+    const buffers = proof[i].left
+      ? [proof[i].data, data]
+      : [data, proof[i].data];
+    data = sha256(Buffer.concat(buffers));
+  }
+  return data;
+}
 
 describe("MerkleTree", function () {
   it("should handle the base case: [A]", function () {
@@ -43,5 +82,36 @@ describe("MerkleTree", function () {
     expect(merkleTree.getRoot()).toEqual(
       "Hash(Hash(Hash(A + B) + Hash(C + D)) + Hash(Hash(E + F) + G))"
     );
+  });
+
+  describe("merkle proof", function () {
+    const leaves = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+    const root =
+      "eb100814abc896ab18bcf6c37b6550eeadeae0c312532286a4cf4be132ace526";
+    const hashTree = new MerkleTree(leaves.map(sha256), concatHash);
+    const lettersTree = new MerkleTree(leaves, concatLetters);
+
+    describe("for each leaf", function () {
+      leaves.forEach((leaf, i) => {
+        if (leaf != "C") return; //debugging
+        it.only(`should return a proof that calculates the root from leaf ${leaves[i]}`, function () {
+          const proof = hashTree.getProof(i);
+          const hashedProof = hashProof(leaf, proof).toString("hex");
+          if (hashedProof !== root) {
+            const lettersProof = lettersTree.getProof(i);
+            console.log(
+              "The resulting hash of your proof is wrong. \n" +
+                `We were expecting: ${root} \n` +
+                `We received: ${hashedProof} \n` +
+                `In ${leaves.join("")} Merkle tree, the proof of ${
+                  leaves[i]
+                } you gave us is: \n` +
+                `${JSON.stringify(lettersProof, null, 2)}`
+            );
+          }
+          expect(hashedProof).toEqual(root);
+        });
+      });
+    });
   });
 });
